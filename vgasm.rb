842ad9481c1b80eb799e6f8a84c3486aa388f1e7
sonota88 <yosiot8753@gmail.com>
2019-05-11 16:05:07 +0900

    git add vgasm.rb

---
+++ vgasm.rb
@@ -0,0 +1,63 @@
+# coding: utf-8
+require 'pp'
+require 'yaml'
+
+def parse(src)
+  alines = []
+  src.each_line do |line|
+    words = line.sub(/#.*/, "").strip.split(/ +/)
+    unless words.empty?
+      alines << words
+    end
+  end
+  alines
+end
+
+def create_label_addr_map(alines)
+  map = {}
+
+  addr = 0
+  alines.each do |aline|
+    head, *rest = aline
+
+    case head
+    when "label"
+      name = rest[0]
+      map[name] = addr
+      addr += 2
+    else
+      addr += 1
+      addr += rest.size
+    end
+  end
+
+  map
+end
+
+src = File.read(ARGV[0])
+alines = parse(src)
+
+# key: ラベル名、 value: アドレス のマッピングを作る
+label_addr_map = create_label_addr_map(alines)
+# pp label_addr_map
+
+words = []
+alines.each do |aline|
+  head, *rest = aline
+
+  words << head
+
+  case head
+  when "label"
+    words << rest[0]
+  when "jump", "jump_eq"
+    label_name = rest[0]
+    words << label_addr_map[label_name]
+  else
+    words += rest.map{ |arg|
+      (/^\-?\d+$/ =~ arg) ? arg.to_i : arg
+    }
+  end
+end
+
+puts YAML.dump(words)
