dac6c3c1f25e6a88da018181b236a4f40f723679
sonota88 <yosiot8753@gmail.com>
2019-12-22 16:44:08 +0900

    refactor: codegen_call_set() から codegen_call() を呼び出すようにした

--- vgcg.rb
+++ vgcg.rb
@@ -239,31 +239,8 @@ def codegen_call_set(fn_arg_names, lvar_names, stmt_rest)
   alines = []
 
   lvar_name, fn_temp = stmt_rest
-  fn_name, *fn_args = fn_temp
 
-  fn_args.reverse.each {|fn_arg|
-    case fn_arg
-    when Integer
-      alines << "  push #{fn_arg}"
-    when String
-      case
-      when fn_arg_names.include?(fn_arg)
-        fn_arg_addr = to_fn_arg_addr(fn_arg_names, fn_arg)
-        alines << "  push #{fn_arg_addr}"
-      when lvar_names.include?(fn_arg)
-        lvar_addr = to_lvar_addr(lvar_names, fn_arg)
-        alines << "  push #{lvar_addr}"
-      else
-        raise not_yet_impl(fn_arg)
-      end
-    else
-      raise not_yet_impl(fn_arg)
-    end
-  }
-
-  alines += codegen_comment("call_set  #{fn_name}")
-  alines << "  call #{fn_name}"
-  alines << "  add_sp #{fn_args.size}"
+  alines += codegen_call(fn_arg_names, lvar_names, fn_temp)
 
   lvar_addr = to_lvar_addr(lvar_names, lvar_name)
   alines << "  cp reg_a #{lvar_addr}"
