87003c832d3f9bbdaa1f4e46cb99c13c32f82688
sonota88 <yosiot8753@gmail.com>
2020-06-29 21:51:13 +0900

    ライフゲームのコード生成のテストを追加

---
+++ test/test_vgcg_gol.rb
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+require_relative "./helper"
+
+class VgcgGolTest < Minitest::Test
+  PROJECT_DIR = File.expand_path("..", __dir__)
+  ASM_FILE = File.join(PROJECT_DIR, "tmp/test_vgasm_gol.vga.txt")
+
+  def test_vgcg_gol
+    system %( ruby #{PROJECT_DIR}/vgcg.rb #{PROJECT_DIR}/test/gol.vgt.json > #{ASM_FILE} )
+
+    assert_equal(
+      File.read("#{PROJECT_DIR}/test/gol.vga.txt"),
+      File.read(ASM_FILE)
+    )
+  end
+end
