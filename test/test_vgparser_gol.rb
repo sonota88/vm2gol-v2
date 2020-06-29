121b208e93fd3de375afb4d1f1a1bfc81aeade18
sonota88 <yosiot8753@gmail.com>
2020-06-29 21:54:56 +0900

    ライフゲームのパースのテストを追加

---
+++ test/test_vgparser_gol.rb
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+require_relative "./helper"
+
+class VgparserGolTest < Minitest::Test
+  PROJECT_DIR = File.expand_path("..", __dir__)
+  TREE_FILE = File.join(PROJECT_DIR, "tmp/test_vgasm_gol.vgt.json")
+
+  def test_vgcg_gol
+    system %( ruby #{PROJECT_DIR}/vgparser.rb #{PROJECT_DIR}/gol.vg.txt > #{TREE_FILE} )
+
+    assert_equal(
+      File.read("#{PROJECT_DIR}/test/gol.vgt.json"),
+      File.read(TREE_FILE)
+    )
+  end
+end
