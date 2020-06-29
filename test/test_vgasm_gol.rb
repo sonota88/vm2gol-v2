8d68011cafac9bbcec46696188fb38e2346823fb
sonota88 <yosiot8753@gmail.com>
2020-06-29 21:43:50 +0900

    ライフゲームのアセンブルのテストを追加

---
+++ test/test_vgasm_gol.rb
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+require_relative "./helper"
+
+class VgasmGolTest < Minitest::Test
+  PROJECT_DIR = File.expand_path("..", __dir__)
+  EXE_FILE = File.join(PROJECT_DIR, "tmp/test_vgasm_gol.vge.yaml")
+
+  def test_vgasm_gol
+    system %( ruby #{PROJECT_DIR}/vgasm.rb #{PROJECT_DIR}/test/gol.vga.txt > #{EXE_FILE} )
+
+    assert_equal(
+      File.read("#{PROJECT_DIR}/test/gol.vge.yaml"),
+      File.read(EXE_FILE)
+    )
+  end
+end
