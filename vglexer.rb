dd90bdee5c022386e611b6429325043dc33f3a53
sonota88 <yosiot8753@gmail.com>
2021-01-17 11:09:55 +0900

    format: 空白行の削除 (vglexer.rb)

--- vglexer.rb
+++ vglexer.rb
@@ -14,40 +14,32 @@ def tokenize(src)
     when /\A([ \n]+)/
       str = $1
       pos += str.size
-
     when %r{\A(//.*)$}
       str = $1
       pos += str.size
-
     when /\A"(.*)"/
       str = $1
       tokens << Token.new(:str, str)
       pos += str.size + 2
-
     when /\A(func|set|var|call_set|call|return|case|while|_cmt)[^a-z_]/
       str = $1
       tokens << Token.new(:kw, str)
       pos += str.size
-
     when /\A(-?[0-9]+)/
       str = $1
       tokens << Token.new(:int, str.to_i)
       pos += str.size
-
     when /\A(==|!=|[(){}=;+*,])/
       str = $1
       tokens << Token.new(:sym, str)
       pos += str.size
-
     when /\A([a-z_][a-z0-9_\[\]]*)/
       str = $1
       tokens << Token.new(:ident, str)
       pos += str.size
-
     else
       p_e rest[0...100]
       raise "must not happen"
-
     end
   end
 
