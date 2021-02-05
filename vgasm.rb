d61db010cc28ee3ebaebb64e1f49f8d5447f94b9
sonota88 <yosiot8753@gmail.com>
2021-02-06 04:19:40 +0900

    format (to_machine_code_operand)

--- vgasm.rb
+++ vgasm.rb
@@ -33,9 +33,9 @@ end
 
 def to_machine_code_operand(arg)
   case arg
-  when /^\[(.+)\]$/      then "ind:#{$1}"
-  when /^-?\d+$/         then arg.to_i
-  else                        arg
+  when /^\[(.+)\]$/ then "ind:#{$1}"
+  when /^-?\d+$/    then arg.to_i
+  else                   arg
   end
 end
 
