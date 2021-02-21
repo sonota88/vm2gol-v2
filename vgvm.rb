69157c2dccb0447b75b7b4bfc4fadab9fcb613e2
sonota88 <yosiot8753@gmail.com>
2021-02-22 06:19:02 +0900

    Self-hosting achieved 🎉

--- vgvm.rb
+++ vgvm.rb
@@ -569,7 +569,7 @@ if $PROGRAM_NAME == __FILE__
     $stdin_ = File.open(stdin_file, "rb")
   end
 
-  stack_size = 90_000
+  stack_size = 3_000_000
   mem = Memory.new(stack_size)
   vm = Vm.new(
     mem,
